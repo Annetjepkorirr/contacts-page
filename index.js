b1e68e2b97134a7285d4f4bb77a22ac827248168
Annetjepkorirr <annetjepkorirr@gmail.com>
2023-07-06 00:55:55 +0300

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// const images = document.querySelectorAll('#phone');
+// images.forEach(image => {
+//     image.addEventListener('click', function() {
+//         images.forEach(img => img.classList.remove('selected'));
+//         this.classList.add('selected');
+//     });
+// });
